d1f16491981e056d47a17d4923069a07ac3ecb10
Angel Tsai <angetsai@umich.edu>
2016-11-22 17:45:17 -0500

    import necessaries

--- Project4.py
+++ Project4.py
@@ -1,1 +1,38 @@
 print ("Project 4: Pygame")
+
+import random
+import sys
+
+import pygame
+from pygame.locals import Rect, DOUBLEBUF, QUIT, K_ESCAPE, KEYDOWN, K_LEFT, K_UP, K_RIGHT, KEYUP, K_LCTRL, K_RETURN, FULLSCREEN
+pygame.init();
+
+#create colors
+white = (255, 255, 255)
+black = (0,0,0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+
+#create a surface
+gameDisplay = pygame.display.set_mode((800,600)) #initialize with a tuple
+
+#lets add a title, aka "caption"
+pygame.display.set_caption("Events")
+
+#pygame.display.flip() 		#similar to a flip book, updates entire surface
+pygame.display.update()		#only updates portion specified
+
+
+gameExit = False
+while not gameExit:
+	for event in pygame.event.get():
+		print(event)
+
+
+
+#required
+pygame.quit()
+quit()	
+
