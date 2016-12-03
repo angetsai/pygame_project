a27a80dc13382433422dc5d3c09dbedd4ad12b26
Angel Tsai <angetsai@umich.edu>
2016-12-03 14:09:17 -0500

    added a rival team, more game-like

--- Project4.py
+++ Project4.py
@@ -39,7 +39,7 @@ class HarryPotter(pygame.sprite.Sprite):
                 self.reverse = 1
         if key[pygame.K_RIGHT]:
             self.rect.x += dist*self.reverse
-            if self.rect.x >= 636:
+            if self.rect.x >= 1270:
                 self.reverse = -1
             if self.rect.x <= 0:
                 self.reverse = 1
@@ -47,13 +47,71 @@ class HarryPotter(pygame.sprite.Sprite):
             self.rect.x -= dist*self.reverse
             if self.rect.x <= 0:
                 self.reverse = -1
-            if self.rect.x >= 847:
+            if self.rect.x >= 1270:
                 self.reverse = 1
                  
         
     def draw(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
         
+        
+class Rival(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('rival.bmp').convert() #enable alpha transparency option  ##look up how to make bmp background transparent
+        self.transColor = self.image.get_at((0,0))
+        self.image.set_colorkey(self.transColor)
+        self.rect = self.image.get_rect()
+        self.radius = self.rect.height/2
+        self.rect.x = 1100
+        self.rect.y = 0
+        self.reverse = 1
+
+    def handle_keys(self):
+        key = pygame.key.get_pressed()
+        dist = randint(1,30)
+        if key[pygame.K_UP]:
+            self.rect.y += dist*self.reverse
+            if self.rect.y >= 636:
+                self.reverse = -1
+            if self.rect.y <= 0:
+                self.reverse = 1
+        elif key[pygame.K_DOWN]:
+            self.rect.y -= dist*self.reverse
+            if self.rect.y <= 0:
+                self.reverse = -1
+            if self.rect.y >= 636:
+                self.reverse = 1
+        if key[pygame.K_LEFT]:
+            self.rect.x += dist*self.reverse
+            if self.rect.x >= 1270:
+                self.reverse = -1
+            if self.rect.x <= 0:
+                self.reverse = 1
+        elif key[pygame.K_RIGHT]:
+            self.rect.x -= dist*self.reverse
+            if self.rect.x <= 0:
+                self.reverse = -1
+            if self.rect.x >= 1270:
+                self.reverse = 1
+                
+        elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    self.rect.y += dist*self.reverse
+                    if self.rect.y >= 636:
+                        self.reverse = -1
+                    if self.rect.y <= 0:
+                        self.reverse = 1
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                    self.rect.x -= dist*self.reverse
+                    if self.rect.x <= 0:
+                        self.reverse = -1
+                    if self.rect.x >= 1270:
+                        self.reverse = 1 
+                 
+        
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))        
 
 
 class Snitch(pygame.sprite.Sprite):
@@ -64,8 +122,8 @@ class Snitch(pygame.sprite.Sprite):
         self.image.set_colorkey(self.transColor)
         self.rect = self.image.get_rect()
         self.radius = self.rect.height/2
-        self.rect.x = 700
-        self.rect.y = 500
+        self.rect.x = 580
+        self.rect.y = 300
         self.reverse = 1
         
           #random movement // design object to move 1 pixel per each instance in while loop 
@@ -88,11 +146,11 @@ class Snitch(pygame.sprite.Sprite):
             self.rect.x -= dist*self.reverse
             if self.rect.x <= 0:
                 self.reverse = -1
-            if self.rect.x >= 847:
+            if self.rect.x >= 1270:
                 self.reverse = 1
         elif key[pygame.K_RIGHT]:
             self.rect.x += dist*self.reverse
-            if self.rect.x >= 636:
+            if self.rect.x >= 1270:
                 self.reverse = -1
             if self.rect.x <= 0:
                 self.reverse = 1
@@ -108,13 +166,11 @@ class Snitch(pygame.sprite.Sprite):
                     self.rect.x -= dist*self.reverse
                     if self.rect.x <= 0:
                         self.reverse = -1
-                    if self.rect.x >= 847:
+                    if self.rect.x >= 1270:
                         self.reverse = 1
             
     def draw(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
-        
-    
         
         
 class Quaffle(pygame.sprite.Sprite):
@@ -130,7 +186,7 @@ class Quaffle(pygame.sprite.Sprite):
         
     def handle_keys(self):
         key = pygame.key.get_pressed()
-        dist = randint(1,7)
+        dist = randint(1,10)
         if key[pygame.K_UP]:
             self.rect.y -= dist*self.reverse
             if self.rect.y <= 0:
@@ -147,11 +203,11 @@ class Quaffle(pygame.sprite.Sprite):
             self.rect.x -= dist*self.reverse
             if self.rect.x <= 0:
                 self.reverse = -1
-            if self.rect.x >= 847:
+            if self.rect.x >= 1270:
                 self.reverse = 1
         elif key[pygame.K_RIGHT]:
             self.rect.x += dist*self.reverse
-            if self.rect.x >= 636:
+            if self.rect.x >= 1270:
                 self.reverse = -1
             if self.rect.x <= 0:
                 self.reverse = 1
@@ -165,7 +221,7 @@ class Quaffle(pygame.sprite.Sprite):
                         self.reverse = 1
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     self.rect.x += dist*self.reverse
-                    if self.rect.x >= 636:
+                    if self.rect.x >= 1270:
                         self.reverse = -1
                     if self.rect.x <= 0:
                         self.reverse = 1
@@ -174,7 +230,7 @@ class Quaffle(pygame.sprite.Sprite):
         surface.blit(self.image, (self.rect.x, self.rect.y))
         
     def move(self):
-        randX = randint(0, 847)
+        randX = randint(0, 1270)
         randY = randint(0, 636)
         self.rect.center = (randX,randY)
         
@@ -185,13 +241,13 @@ class Bludger1(pygame.sprite.Sprite):
         self.transColor = self.image.get_at((0,0))
         self.image.set_colorkey(self.transColor)
         self.rect = self.image.get_rect()
-        self.rect.x = 320
-        self.rect.y = 280
+        self.rect.x = 550
+        self.rect.y = 200
         self.reverse = 1
         
     def handle_keys(self):
         key = pygame.key.get_pressed()
-        dist = randint(1,30)
+        dist = randint(1,25)
         if key[pygame.K_UP]:
             self.rect.y += dist*self.reverse
             if self.rect.y >= 636:
@@ -206,7 +262,7 @@ class Bludger1(pygame.sprite.Sprite):
                 self.reverse = 1
         if key[pygame.K_LEFT]:
             self.rect.x += dist*self.reverse
-            if self.rect.x >= 636:
+            if self.rect.x >= 1270:
                 self.reverse = -1
             if self.rect.x <= 0:
                 self.reverse = 1
@@ -214,7 +270,7 @@ class Bludger1(pygame.sprite.Sprite):
             self.rect.x -= dist*self.reverse
             if self.rect.x <= 0:
                 self.reverse = -1
-            if self.rect.x >= 847:
+            if self.rect.x >= 1270:
                 self.reverse = 1
                 
         elif event.type == pygame.KEYUP:
@@ -226,7 +282,7 @@ class Bludger1(pygame.sprite.Sprite):
                     self.reverse = 1
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     self.rect.x += dist*self.reverse
-                    if self.rect.x >= 636:
+                    if self.rect.x >= 1270:
                         self.reverse = -1
                     if self.rect.x <= 0:
                         self.reverse = 1  
@@ -235,7 +291,7 @@ class Bludger1(pygame.sprite.Sprite):
         surface.blit(self.image, (self.rect.x, self.rect.y))
         
     def move(self):
-        randX = randint(0, 847)
+        randX = randint(0, 1270)
         randY = randint(0, 636)
         self.rect.center = (randX,randY)
         
@@ -247,12 +303,12 @@ class Bludger2(pygame.sprite.Sprite):
         self.image.set_colorkey(self.transColor)
         self.rect = self.image.get_rect()
         self.rect.x = 700
-        self.rect.y = 20
+        self.rect.y = 400
         self.reverse = 1
 
     def handle_keys(self):
         key = pygame.key.get_pressed()
-        dist = randint(1,30)
+        dist = randint(1,25)
         if key[pygame.K_UP]:
             self.rect.y += dist*self.reverse
             if self.rect.y >= 636:
@@ -267,7 +323,7 @@ class Bludger2(pygame.sprite.Sprite):
                 self.reverse = 1
         if key[pygame.K_LEFT]:
             self.rect.x += dist*self.reverse
-            if self.rect.x >= 636:
+            if self.rect.x >= 1270:
                 self.reverse = -1
             if self.rect.x <= 0:
                 self.reverse = 1
@@ -275,7 +331,7 @@ class Bludger2(pygame.sprite.Sprite):
             self.rect.x -= dist*self.reverse
             if self.rect.x <= 0:
                 self.reverse = -1
-            if self.rect.x >= 847:
+            if self.rect.x >= 1270:
                 self.reverse = 1
                 
         elif event.type == pygame.KEYUP:
@@ -289,21 +345,21 @@ class Bludger2(pygame.sprite.Sprite):
                     self.rect.x -= dist*self.reverse
                     if self.rect.x <= 0:
                         self.reverse = -1
-                    if self.rect.x >= 847:
+                    if self.rect.x >= 1270:
                         self.reverse = 1  
             
     def draw(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
         
     def move(self):
-        randX = randint(0, 847)
+        randX = randint(0, 1270)
         randY = randint(0, 636)
         self.rect.center = (randX,randY)
         
 
       
 # set window size to match picture size
-width = 847
+width = 1270
 height = 636
 
 # initialise pygame
@@ -315,6 +371,7 @@ pygame.display.set_caption("Hogwarts")
 #load picture
 background= pygame.image.load('hogwarts.bmp')
 hp = HarryPotter()
+rival = Rival()
 snitch = Snitch()
 quaffle = Quaffle()
 bludger1 = Bludger1()
@@ -325,6 +382,7 @@ mouse.set_visible(False)
 f = pygame.font.SysFont(None, 25)
 
 hits = 0
+comp = 0
 
 clock = pygame.time.Clock()
 crashed = False
@@ -339,108 +397,16 @@ while not gameExit:
         if event.type == pygame.QUIT:
             gameExit = True
             
-        #movement for harry    
-        
-        #elif event.type == pygame.KEYDOWN:
-        #    dist = 10
-        #    if event.key == pygame.K_UP:
-        #        hp.rect.y -= dist
-        #    if event.key == pygame.K_DOWN:
-        #        hp.rect.y += dist
-        #    if event.key == pygame.K_RIGHT:
-        #        hp.rect.x += dist
-        #    if event.key == pygame.K_LEFT:
-        #        hp.rect.x -= dist
-                
-        #    elif event.type == pygame.KEYUP:
-        #        if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-        #            hp.rect.y = 0
-        #        if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-        #            hp.rect.x = 0
-                    
-            # movement of snitch
-        #    dist = randint(1,15)
-        #    if event.key == pygame.K_UP:
-        #        snitch.rect.y -= dist
-        #    if event.key == pygame.K_DOWN:
-        #        snitch.rect.y += dist
-        #    if event.key == pygame.K_RIGHT:
-        #        snitch.rect.x += dist
-        #    if event.key == pygame.K_LEFT:
-        #        snitch.rect.x -= dist
-                
-        #    elif event.type == pygame.KEYUP:
-        #        if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-        #            snitch.rect.y += dist
-        #        if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-        #            snitch.rect.x -= dist 
-                    
-            # movement of quaffle
-        #    dist = randint(1,7)
-        #    if event.key == pygame.K_UP:
-        #        quaffle.rect.y += dist
-        #    if event.key == pygame.K_DOWN:
-        #        quaffle.rect.y -= dist
-        #    if event.key == pygame.K_RIGHT:
-        #        quaffle.rect.x -= dist
-        #    if event.key == pygame.K_LEFT:
-        #        quaffle.rect.x += dist
-                
-        #    elif event.type == pygame.KEYUP:
-        #        if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-        #            quaffle.rect.y -= dist
-        #        if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-        #            quaffle.rect.x += dist   
-    
-            # movement of bludger1    
-        #    dist = randint(1,30)
-        #    if event.key == pygame.K_UP:
-        #        bludger1.rect.y -= dist
-        #    if event.key == pygame.K_DOWN:
-        #        bludger1.rect.y += dist
-        #    if event.key == pygame.K_RIGHT:
-        #        bludger1.rect.x += dist
-        #    if event.key == pygame.K_LEFT:
-        #        bludger1.rect.x -= dist
-                
-        #    elif event.type == pygame.KEYUP:
-        #        if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-        #            bludger1.rect.y -= dist
-        #        if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-        #            bludger1.rect.x += dist  
-        
-            # movement of bludger2
-        #    dist = randint(1,30)
-        #    if event.key == pygame.K_UP:
-        #        bludger2.rect.y -= dist
-        #    if event.key == pygame.K_DOWN:
-        #        bludger2.rect.y += dist
-        #    if event.key == pygame.K_RIGHT:
-        #        bludger2.rect.x += dist
-        #    if event.key == pygame.K_LEFT:
-        #        bludger2.rect.x -= dist
-                
-        #    elif event.type == pygame.KEYUP:
-        #        if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-        #            bludger2.rect.y += dist
-        #        if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-        #            bludger2.rect.x -= dist  
-            
-            
-            #hp.handle_keys()
-            #snitch.handle_keys()
-            #quaffle.handle_keys()
-            #bludger1.handle_keys()
-            #bludger2.handle_keys()
+
     if pygame.sprite.collide_rect(hp, quaffle):
-        #mixer.Sound("c.wav").play()
+        mixer.Sound("metal-clang.wav").play()
         hits += 10
         quaffle.move()
     
     if pygame.sprite.collide_circle(hp, snitch):
         hits += 150
         
-        print ('You won the Quidditch game')
+        print ('The Quidditch game has ended')
         gameExit = True
         
     if pygame.sprite.collide_rect(hp, bludger1):
@@ -453,29 +419,59 @@ while not gameExit:
         hits -= 15
         bludger2.move()
             
+    if pygame.sprite.collide_rect(rival, quaffle):
+        comp += 10
+        quaffle.move()
+    
+    if pygame.sprite.collide_rect(rival, snitch):
+        comp += 150
+        print ('The Quidditch game has ended')
+        gameExit = True
+        
+    if pygame.sprite.collide_rect(rival, bludger1):
+        comp -= 15
+        bludger1.move()
+        
+    if pygame.sprite.collide_rect(rival, bludger2):
+        comp -= 15
+        bludger2.move()
     
      
 
     hp.handle_keys()
-    snitch.handle_keys() # have these again to make movement more smooth
+    rival.handle_keys()
+    snitch.handle_keys() 
     quaffle.handle_keys()
     bludger1.handle_keys()
     bludger2.handle_keys()
     screen.blit(background,(0,0))
     hp.draw(screen)
+    rival.draw(screen)
     snitch.draw(screen)
     quaffle.draw(screen)
     bludger1.draw(screen)
     bludger2.draw(screen)
     
     
-    text = f.render("Score = " + str(hits), True, (0,0,0))
-    screen.blit(text, (380, 0))
+    text = f.render("Your Score = " + str(hits), True, (0,0,0))
+    screen.blit(text, (400, 0))
+    text2 = f.render("Opponent's Score = " + str(comp), True, (0,0,0))
+    screen.blit(text2, (700, 0))
     if gameExit == True:
-        text_end = f.render("You caught the snitch! YOU WIN", True, (255,255,255))
-        mixer.Sound("cheers.wav").play()
-        screen.blit(text_end, (350, 300))
-        pygame.display.update()
+        if hits > comp:
+            text_win = f.render("The snitch has been caught and YOU WIN", True, (255,255,255))
+            mixer.Sound("cheers.wav").play()
+            screen.blit(text_win, (500, 300))
+            pygame.display.update()
+        elif comp > hits:
+            text_lose = f.render("The snitch has been caught but YOU LOSE", True, (255,255,255))
+            mixer.Sound("crowd-boo.wav").play()
+            screen.blit(text_lose, (500, 300))
+            pygame.display.update()
+        elif comp == hits:
+            text_tie = f.render ("This game has ended in a tie", True, (255,255,255))
+            screen.blit(text_tie, (500, 300))
+            pygame.display.update()
         pygame.time.delay(3000)
         break
         
